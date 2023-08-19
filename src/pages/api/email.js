19b92b2658934c548ac9b7c0356a737048f4bb87
Eli Sailer-Haugland <eli092011@gmail.com>
2023-08-18 19:37:29 -0500

    email

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -1,9 +1,9 @@
+import { resolve } from "styled-jsx/css"
+
 var nodemailer = require("nodemailer")
 
 export default async function  handler(req, res) {
     const {email, name, message}= req.body
-          console.log(process.env.EMAIL)
-          console.log(process.env.EMAIL_PASSWORD)
     try{
     var transporter = nodemailer.createTransport({
       service:"Gmail",
@@ -12,6 +12,19 @@ export default async function  handler(req, res) {
         pass:process.env.EMAIL_PASSWORD
       }
     })
+    await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
+
     var mailOptions = {
       from:email,
       to:process.env.EMAIL,
@@ -24,12 +37,16 @@ export default async function  handler(req, res) {
       ${message}
       `
     }
-
-    transporter.sendMail(mailOptions, (error, info)=>{
+await new Promise((res, rej)=>{
+  transporter.sendMail(mailOptions, (error, info)=>{
       if(error){
-        console.log(error)
+       reject(error)
+      } else{
+        resolve(info)
       }
     })
+})
+  
     return res.status(200).json({status:"Success"})
   }
   catch(error){
