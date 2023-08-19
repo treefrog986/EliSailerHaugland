b704df50144f61532ef201b431692625f9e7f284
Eli Sailer-Haugland <eli092011@gmail.com>
2023-08-18 19:05:00 -0500

    changes

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -2,6 +2,8 @@ var nodemailer = require("nodemailer")
 
 export default async function  handler(req, res) {
     const {email, name, message}= req.body
+          console.log(process.env.EMAIL)
+          console.log(process.env.EMAIL_PASSWORD)
     try{
     var transporter = nodemailer.createTransport({
       service:"Gmail",
